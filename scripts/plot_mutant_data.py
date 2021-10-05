b11c22ca870d387f8e807e384a98ff5bd80864b2
Vasu Vikram <vvikram@andrew.cmu.edu>
2021-10-05 11:33:15 -0400

    plot against trials

--- scripts/plot_mutant_data.py
+++ scripts/plot_mutant_data.py
@@ -7,12 +7,15 @@ def read_plot_data(input_plot_data):
 
 
 def plot_mutants(df, output_file):
-    plt.plot(df['total_time'], df['found_muts'], label="Found Mutants")
-    plt.plot(df['total_time'], df['seen_muts'], label="Seen Mutants")
-    plt.plot(df['total_time'], df['dead_muts'], label="Killed Mutants")
-    plt.plot(df['total_time'], df['run_muts'], label="Run Mutants (moving average)")
+    plt.plot(df.index, df['found_muts'], label="Found Mutants")
+    if 'seen_muts' in df.columns:
+        plt.plot(df.index, df['seen_muts'], label="Seen Mutants")
+    if 'infected_muts' in df.columns:
+        plt.plot(df.index, df['infected_muts'], label="Infected Mutants")
+    plt.plot(df.index, df['dead_muts'], label="Killed Mutants")
+    plt.plot(df.index, df['run_muts'], label="Run Mutants (moving average)")
     plt.title("Mutant Plots")
-    plt.xlabel("Total Time (ms)")
+    plt.xlabel("Trials")
     plt.ylabel("Number of Mutants")
     plt.legend(loc='upper right', bbox_to_anchor=(1.6, 1.02), fancybox=True)
     plt.savefig(output_file, bbox_inches='tight')
