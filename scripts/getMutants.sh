46b9224a87fbbcd21243152085122d167a6678ff
Vasu Vikram <vvikram@andrew.cmu.edu>
2021-10-22 11:16:42 -0400

    updated getMutants and venn script with more stats

--- scripts/getMutants.sh
+++ scripts/getMutants.sh
@@ -5,32 +5,35 @@
 #  ./getMutants.sh edu.berkeley.cs.jqf.examples.commons.PatriciaTrieTest testPrefixMap org.apache.commons.collections4.trie 3 1000 ../../jqf/examples
 
 getResults() {
-    mvn jqf:fuzz -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dout=tmpZest -Dengine=zest -Dtrials=$5
-    mvn jqf:fuzz -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dout=tmpMu2 -Dengine=mutation -Dtrials=$5
+    mvn jqf:fuzz -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dout=tmpZest/exp_$4 -Dengine=zest -Dtrials=$5
+    mvn jqf:fuzz -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dout=tmpMu2/exp_$4 -Dengine=mutation -Dtrials=$5
 
-    mvn jqf:mutate -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dinput=target/tmpZest/corpus > zest-results.txt
-    mvn jqf:mutate -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dinput=target/tmpMu2/corpus > mutate-results.txt
+    # Debug purposes - dump args to look at actual files
+    mvn jqf:repro -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dinput=target/tmpZest/exp_$4/corpus -DdumpArgsDir=target/tmpZest/exp_$4/args_corpus/
+    mvn jqf:repro -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dinput=target/tmpMu2/exp_$4/corpus -DdumpArgsDir=target/tmpMu2/exp_$4/args_corpus/
 
-    cat zest-results.txt | grep -v "SUCCESS" | grep "FAILURE" -B 1 | grep -v "FAILURE" | grep -v "\-\-" > filters/zest-filter$4.txt
-    cat mutate-results.txt | grep -v "SUCCESS" | grep "FAILURE" -B 1 | grep -v "FAILURE" | grep -v "\-\-" > filters/mutate-filter$4.txt
+    mvn jqf:mutate -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dinput=target/tmpZest/corpus > results/zest-results-$4.txt
+    mvn jqf:mutate -Dclass=$1 -Dmethod=$2 -Dincludes=$3 -Dinput=target/tmpMu2/corpus > results/mutate-results-$4.txt
+
+    cat results/zest-results-$4.txt | grep "Running Mutant\|FAILURE" > filters/zest-filter-$4.txt
+    cat results/mutate-results-$4.txt | grep "Running Mutant\|FAILURE" > filters/mutate-filter-$4.txt
 }
 
 CURDIR=$(pwd)
 cd $6
 mkdir filters
+mkdir results
 
 for i in $(seq 1 1 $4)
 do
     getResults $1 $2 $3 $i $5
 done
 
-python3 $CURDIR/venn.py $4
+cd $CURDIR
+python venn.py --filters_dir $6/filters --num_experiments $4 --output_img venn.png
 
 #comment the below lines to not remove the created files
-rm zest-results.txt
-rm mutate-results.txt
-rm -r filters
-rm -r target/tmpZest
-rm -r target/tmpMu2
-
-cd $CURDIR
+# rm -r filters
+# rm -r results
+# rm -r target/tmpZest
+# rm -r target/tmpMu2
--- scripts/venn.py
+++ scripts/venn.py
@@ -1,39 +1,91 @@
 from matplotlib_venn import venn2
-from matplotlib.pyplot import savefig
-from sys import argv
-from numpy import add
+from scipy import stats
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
 
 prefix = "[INFO] Running Mutant "
 trim = (lambda strng : strng[len(prefix):-1])
 
-def getVals(i):
-    zestFile = open("filters/zest-filter" + str(i) + ".txt", "r")
-    mutateFile = open("filters/mutate-filter" + str(i) + ".txt", "r")
+def get_killed_mutants(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
 
-    zestLs = set([trim(strng) for strng in zestFile.readlines()])
-    mutateLs = set([trim(strng) for strng in mutateFile.readlines()])
+    i = 0
+    killed_mutants = set()
+    while i < len(lines):
+        if "Running Mutant" in lines[i]:
+            killed = False
+            mutant_name = lines[i][lines[i].index("Mutant"):].strip()
+            i += 1
+            while i < len(lines) and "Running Mutant" not in lines[i]:
+                if "FAILURE" in lines[i]:
+                    killed = True
+                    i += 1
+        if killed:
+            killed_mutants.add(mutant_name)
+    return killed_mutants
 
-    zestUniq = zestLs - mutateLs
-    mutateUniq = mutateLs - zestLs
-    bothFound = zestLs.intersection(mutateLs)
+def get_unique(zest_killed, mutate_killed):
+
+    zest_uniq = zest_killed - mutate_killed
+    mutate_uniq = mutate_killed - zest_killed
+    both_found = zest_killed.intersection(mutate_killed)
 
     print("\nUnique to Zest:")
-    print(*list(zestUniq), sep = "\n")
+    print(*list(zest_uniq), sep = "\n")
     print("\nUnique to Mu2:")
-    print(*list(mutateUniq), sep = "\n")
-    print("\nBoth found another", len(bothFound))
-    
-    return len(zestUniq), len(mutateUniq), len(bothFound)
+    print(*list(mutate_uniq), sep = "\n")
+    print("\nBoth found another", len(both_found))
 
-av0 = int(argv[1])
-zestUniques = 0.0
-mutateUniques = 0.0
-bothFoundElems = 0.0
-for i in range(1, av0 + 1):
-    zU, mU, bF = getVals(i)
-    zestUniques += zU
-    mutateUniques += mU
-    bothFoundElems += bF
+    return len(zest_uniq), len(mutate_uniq), len(both_found)
 
-fig = venn2(subsets = (round(100 * zestUniques / av0) / 100.0, round(100 * mutateUniques / av0) / 100.0, round(100 * bothFoundElems / av0) / 100.0), set_labels = ["Zest           ", "           Mu2"])
-savefig("venn.png")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filters_dir", required=True, type=str)
+    parser.add_argument("--num_experiments", required=True, type=int)
+    parser.add_argument("--output_img", required=True, type=str)
+
+    args = parser.parse_args()
+    zest_killed = []
+    mutate_killed = []
+
+    zest_unique_count = []
+    mutate_unique_count = []
+    both_found = []
+    for i in range(1, args.num_experiments + 1):
+        zest = get_killed_mutants(os.path.join(args.filters_dir, "zest-filter-" + str(i) + ".txt"))
+        mutate = get_killed_mutants(os.path.join(args.filters_dir, "mutate-filter-" + str(i) + ".txt"))
+
+        zest_killed.append(len(zest))
+        mutate_killed.append(len(mutate))
+
+        zest_unique, mutate_unique, both = get_unique(zest, mutate)
+
+        zest_unique_count.append(zest_unique)
+        mutate_unique_count.append(mutate_unique)
+        both_found.append(both)
+
+    print("---------------------------------------------------\n\n")
+    print("Zest Killed Counts: ")
+    print(zest_killed)
+    print("Mu2 Killed Counts: ")
+    print(mutate_killed)
+    print("Zest Mutant Killed Summary: ")
+    print(stats.describe(zest_killed))
+    print("Mu2 Mutant Killed Summary: ")
+    print(stats.describe(mutate_killed))
+    print("Zest Unique Mutant Killed Summary: ")
+    print(stats.describe(zest_unique_count))
+    print("Mu2 Unique Mutant Killed Summary: ")
+    print(stats.describe(mutate_unique_count))
+    print("Both Killed Summary: ")
+    print(stats.describe(both_found))
+    print("T-test between Killed Counts: ")
+    print(stats.ttest_ind(zest_killed, mutate_killed))
+
+
+    fig = venn2(subsets = (np.mean(zest_unique_count), np.mean(mutate_unique_count), np.mean(both_found)), set_labels = ["Zest           ", "           Mu2"])
+    plt.savefig(args.output_img)
+
