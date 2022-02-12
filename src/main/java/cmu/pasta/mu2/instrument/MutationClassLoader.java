1f8bea501696043a1112f347503efe28c4486eb0
Bella Laybourn <ilaybour@andrew.cmu.edu>
2022-02-12 11:08:24 -0500

    instrument all MCL-loaded classes for timeout checks

--- src/main/java/cmu/pasta/mu2/instrument/MutationClassLoader.java
+++ src/main/java/cmu/pasta/mu2/instrument/MutationClassLoader.java
@@ -49,102 +49,106 @@ public class MutationClassLoader extends URLClassLoader {
       throw new ClassNotFoundException("I/O exception while loading class.", e);
     }
 
-    if (name.equals(this.mutationInstance.className)) {
-      AtomicLong found = new AtomicLong(0);
-      ClassReader cr = new ClassReader(bytes);
-      ClassWriter cw = new SafeClassWriter(cr, this,
-          ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-      cr.accept(new ClassVisitor(Mutator.cvArg, cw) {
-        @Override
-        public MethodVisitor visitMethod(int access, String name, String signature,
-            String superName,
-            String[] interfaces) {
-          return new MethodVisitor(Mutator.cvArg,
-              cv.visitMethod(access, name, signature, superName, interfaces)) {
-            @Override
-            public void visitJumpInsn(int opcode, Label label) {
-              // Increment timer and check for time outs at each jump instruction
-              mv.visitLdcInsn(mutationInstance.id);
-              mv.visitMethodInsn(Opcodes.INVOKESTATIC,
-                  Type.getInternalName(MutationSnoop.class),
-                  "checkTimeout", "(I)V", false);
+    String findingClass = name;
+    AtomicLong found = new AtomicLong(0);
+    ClassReader cr = new ClassReader(bytes);
+    ClassWriter cw = new SafeClassWriter(cr, this,
+        ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+    cr.accept(new ClassVisitor(Mutator.cvArg, cw) {
+      @Override
+      public MethodVisitor visitMethod(int access, String name, String signature,
+          String superName,
+          String[] interfaces) {
+        return new MethodVisitor(Mutator.cvArg,
+            cv.visitMethod(access, name, signature, superName, interfaces)) {
+          @Override
+          public void visitJumpInsn(int opcode, Label label) {
+            // Increment timer and check for time outs at each jump instruction
+            mv.visitLdcInsn(mutationInstance.id);
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC,
+                Type.getInternalName(MutationSnoop.class),
+                "checkTimeout", "(I)V", false);
 
-              // Increment offset if the mutator matches
-              if (mutationInstance.mutator.isOpportunity(opcode, signature)
-                  && found.getAndIncrement() == mutationInstance.sequenceIdx) {
-                // Mutator and offset match, so perform mutation
-                for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
-                  ic.call(mv, label);
-                }
-              } else {
-                // No mutation
-                super.visitJumpInsn(opcode, label);
+            // Increment offset if the mutator matches
+            if (findingClass.equals(mutationInstance.className)
+                && mutationInstance.mutator.isOpportunity(opcode, signature)
+                && found.getAndIncrement() == mutationInstance.sequenceIdx) {
+              // Mutator and offset match, so perform mutation
+              for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
+                ic.call(mv, label);
               }
+            } else {
+              // No mutation
+              super.visitJumpInsn(opcode, label);
             }
+          }
 
-            @Override
-            public void visitLdcInsn(Object value) {
-              // Increment offset if the mutator matches
-              if (mutationInstance.mutator.isOpportunity(Opcodes.LDC, signature)
-                  && found.getAndIncrement() == mutationInstance.sequenceIdx) {
-                // Mutator and offset match, so perform mutation
-                for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
-                  ic.call(mv, null);
-                }
-              } else {
-                // No mutation
-                super.visitLdcInsn(value);
+          @Override
+          public void visitLdcInsn(Object value) {
+            // Increment offset if the mutator matches
+            if (findingClass.equals(mutationInstance.className)
+                && mutationInstance.mutator.isOpportunity(Opcodes.LDC, signature)
+                && found.getAndIncrement() == mutationInstance.sequenceIdx) {
+              // Mutator and offset match, so perform mutation
+              for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
+                ic.call(mv, null);
               }
+            } else {
+              // No mutation
+              super.visitLdcInsn(value);
             }
+          }
 
-            @Override
-            public void visitIincInsn(int var, int increment) {
-              // Increment offset if the mutator matches
-              if (mutationInstance.mutator.isOpportunity(Opcodes.IINC, signature)
-                  && found.getAndIncrement() == mutationInstance.sequenceIdx) {
-                // Mutator and offset match, so perform mutation
-                super.visitIincInsn(var, -increment); // TODO: Why is this hardcoded?
-              } else {
-                // No mutation
-                super.visitIincInsn(var, increment);
-              }
+          @Override
+          public void visitIincInsn(int var, int increment) {
+            // Increment offset if the mutator matches
+            if (findingClass.equals(mutationInstance.className)
+                && mutationInstance.mutator.isOpportunity(Opcodes.IINC, signature)
+                && found.getAndIncrement() == mutationInstance.sequenceIdx) {
+              // Mutator and offset match, so perform mutation
+              super.visitIincInsn(var, -increment); // TODO: Why is this hardcoded?
+            } else {
+              // No mutation
+              super.visitIincInsn(var, increment);
             }
+          }
 
-            @Override
-            public void visitMethodInsn(int opcode, String owner, String name, String descriptor,
-                boolean isInterface) {
-              // Increment offset if the mutator matches
-              if (mutationInstance.mutator.isOpportunity(opcode, descriptor)
-                  && found.getAndIncrement() == mutationInstance.sequenceIdx) {
-                // Mutator and offset match, so perform mutation
-                for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
-                  ic.call(mv, null);
-                }
-              } else {
-                // No mutation
-                super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+          @Override
+          public void visitMethodInsn(int opcode, String owner, String name, String descriptor,
+              boolean isInterface) {
+            // Increment offset if the mutator matches
+            if (findingClass.equals(mutationInstance.className)
+                && mutationInstance.mutator.isOpportunity(opcode, descriptor)
+                && found.getAndIncrement() == mutationInstance.sequenceIdx) {
+              // Mutator and offset match, so perform mutation
+              for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
+                ic.call(mv, null);
               }
+            } else {
+              // No mutation
+              super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
             }
+          }
 
-            @Override
-            public void visitInsn(int opcode) {
-              // Increment offset if the mutator matches
-              if (mutationInstance.mutator.isOpportunity(opcode, signature)
-                  && found.getAndIncrement() == mutationInstance.sequenceIdx) {
-                // Mutator and offset match, so perform mutation
-                for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
-                  ic.call(mv, null);
-                }
-              } else {
-                // No mutation
-                super.visitInsn(opcode);
+          @Override
+          public void visitInsn(int opcode) {
+            // Increment offset if the mutator matches
+            if (findingClass.equals(mutationInstance.className)
+                && mutationInstance.mutator.isOpportunity(opcode, signature)
+                && found.getAndIncrement() == mutationInstance.sequenceIdx) {
+              // Mutator and offset match, so perform mutation
+              for (InstructionCall ic : mutationInstance.mutator.replaceWith()) {
+                ic.call(mv, null);
               }
+            } else {
+              // No mutation
+              super.visitInsn(opcode);
             }
-          };
-        }
-      }, 0);
-      bytes = cw.toByteArray();
-    }
+          }
+        };
+      }
+    }, 0);
+    bytes = cw.toByteArray();
 
     return defineClass(name, bytes, 0, bytes.length);
   }
