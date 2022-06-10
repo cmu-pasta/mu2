3340f528ea5a9b8dd4a09714b7c5e305ffd32d79
Nicole Nair <nicoleraenair@gmail.com>
2022-06-10 15:15:24 -0400

    Remove unnecessary import statements

--- src/main/java/cmu/pasta/mu2/fuzz/RandomMutationGuidance.java
+++ src/main/java/cmu/pasta/mu2/fuzz/RandomMutationGuidance.java
@@ -1,34 +1,14 @@
 package cmu.pasta.mu2.fuzz;
 
-import cmu.pasta.mu2.MutationInstance;
-import cmu.pasta.mu2.diff.DiffException;
-import cmu.pasta.mu2.diff.Outcome;
-import cmu.pasta.mu2.util.Serializer;
-import cmu.pasta.mu2.diff.guidance.DiffGuidance;
-import cmu.pasta.mu2.diff.junit.DiffTrialRunner;
 import cmu.pasta.mu2.instrument.MutationClassLoaders;
-import cmu.pasta.mu2.instrument.MutationSnoop;
-import cmu.pasta.mu2.instrument.OptLevel;
 import cmu.pasta.mu2.util.ArraySet;
-import edu.berkeley.cs.jqf.fuzz.ei.ZestGuidance;
-import edu.berkeley.cs.jqf.fuzz.guidance.GuidanceException;
-import edu.berkeley.cs.jqf.fuzz.guidance.Result;
-import edu.berkeley.cs.jqf.fuzz.util.MovingAverage;
-import edu.berkeley.cs.jqf.instrument.InstrumentationException;
 import cmu.pasta.mu2.diff.guidance.OptimizedMutationGuidance;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.time.Duration;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Objects;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-import org.junit.runners.model.FrameworkMethod;
-import org.junit.runners.model.TestClass;
 import java.util.Collections;
 
 public class RandomMutationGuidance extends MutationGuidance implements OptimizedMutationGuidance{
