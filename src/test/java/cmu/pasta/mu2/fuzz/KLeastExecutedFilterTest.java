d4d0b0949951a0ff47ad7423148e1d9e8eba678c
Nicole Nair <nicoleraenair@gmail.com>
2022-06-17 11:30:57 -0400

    Add tests for LeastExecutedFilter

--- src/test/java/cmu/pasta/mu2/fuzz/KLeastExecutedFilterTest.java
+++ src/test/java/cmu/pasta/mu2/fuzz/KLeastExecutedFilterTest.java
@@ -2,7 +2,9 @@ package cmu.pasta.mu2.fuzz;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import cmu.pasta.mu2.MutationInstance;
 import cmu.pasta.mu2.instrument.Mutator;
@@ -55,6 +57,71 @@ public class KLeastExecutedFilterTest {
         List<MutationInstance> filteredList = filter.filterMutants(toFilter);
         // filtered list should be of same size as toFilter
         assertEquals(toFilter.size(), filteredList.size());
+    }
+
+    /*
+     * 
+     */
+    @Test
+    public void leastExecutedLogicWorks(){
+
+        Set<MutationInstance> newMutants = new HashSet<>(Arrays.asList(
+            new MutationInstance("New1", Mutator.I_ADD_TO_SUB, 6, 1, "New1.java"),
+            new MutationInstance("New2", Mutator.ARETURN_TO_NULL, 7, 1, "New2.java"),
+            new MutationInstance("New3", Mutator.I_DIV_TO_MUL, 8, 1, "New3.java")
+        ));
+
+        KLeastExecutedFilter filter = new KLeastExecutedFilter(5);
+
+        // Filter original mutants so that all are executed once
+        List<MutationInstance> originalMutants = filter.filterMutants(toFilter);
+        // All mutants should be contained in filtered list
+        assertEquals(5, originalMutants.size());
+        // Add new mutants to combined list
+        List<MutationInstance> allMutants = new ArrayList<>(toFilter);
+        allMutants.addAll(newMutants);
+
+        // Filter list again
+        List<MutationInstance> filtered1 = filter.filterMutants(allMutants);
+        // New (never-executed) mutants should all be in filtered list to be executed
+        assertTrue(filtered1.containsAll(newMutants));
+        // Count number of original mutants in filtered list, add to set of mutants that have been executed twice
+        int count = 0;
+        Set<MutationInstance> executedTwice = new HashSet<>();
+        for (MutationInstance mutant: filtered1){
+            if (originalMutants.contains(mutant)) {
+                count += 1;
+                executedTwice.add(mutant);
+            }
+        }
+        // Filtered list should contain 5-3=2 original mutants
+        assertEquals(2, count);
+
+        // Filter list again
+        List<MutationInstance> filtered2 = filter.filterMutants(allMutants);
+        // Filtered list should not contain any of the 2 mutants that have been executed twice
+        for (MutationInstance mutant : executedTwice) {
+            assertFalse(filtered2.contains(mutant));
+        }
+        // Add all recently executed mutants to executedTwice
+        for (MutationInstance mutant : filtered2) {
+            executedTwice.add(mutant);
+        }
+        // All mutants except 1 should now have been executed twice
+        assertEquals(7,executedTwice.size());
+
+        // Filter list again
+        List<MutationInstance> filtered3 = filter.filterMutants(allMutants);
+        // The 1 remaining once-executed should be contained (the rest have all been executed twice)
+        boolean found = false;
+        for (MutationInstance mutant : filtered3) {
+            if (!executedTwice.contains(mutant)){
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+
     }
 }
 
