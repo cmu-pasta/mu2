2b94a21b121bd1b5a1221537b23619dccb6816a7
Vasu Vikram <vvikram@andrew.cmu.edu>
2022-07-06 19:07:36 -0400

    latets remote script

--- scripts/fuzz_remote.sh
+++ scripts/fuzz_remote.sh
@@ -8,23 +8,41 @@ if [ $# -lt 4 ]; then
 fi
 
 PEMPATH="$1" # example: ~/aws_keys/key.pem
-INSTANCE_ID="$2"
-CONFIG="$3" # one of "zest", "mu2", "mu2nosave"
-REMOTE_RESULTS_DIR="$4"
-ARGS="${@:5}"
+CONFIG="$2" # one of "zest", "mu2", "mu2nosave"
+BENCHMARK="$3"
+REMOTE_RESULTS_DIR="/home/ubuntu/sort-benchmarks/target/$BENCHMARK-fuzz-results/$CONFIG"
+INSTANCES_FILE="$4"
+PROCS="$5"
+ARGS="${@:6}"
 
-aws ec2 start-instances --instance-ids $INSTANCE_ID
-aws ec2 wait instance-running --instance-ids $INSTANCE_ID
-IP=`aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text`
-REMOTE="ubuntu@$IP"
 
-# Make sure it is initialized
-scp -i $PEMPATH github_key $REMOTE:.ssh/id_rsa
-scp -i $PEMPATH known_hosts $REMOTE:.ssh/known_hosts
-scp -i $PEMPATH -r $HOME/Work/fuzz_chocopy/src/main/java/chocopy/reference/ $REMOTE:
-ssh -i $PEMPATH $REMOTE "bash -s" < init.sh
+function runFuzzing() {
 
-# Run fuzzing sessions (with repetitions) using multiple screens
-ssh -i $PEMPATH $REMOTE FUZZ_CONFIG=$CONFIG FUZZ_ARGS=\"$ARGS\" "bash -s" < fuzz_run.sh
-scp -i $PEMPATH -r $REMOTE:$REMOTE_RESULTS_DIR/* $HOME/mu2_remote_results/$CONFIG/
-aws ec2 stop-instances --instance-ids $INSTANCE_ID
+  INSTANCE_ID=$1
+  EXPERIMENT=$2
+	echo "Running on instance $INSTANCE_ID for starting expeirment $EXPERIMENT"
+  aws ec2 start-instances --instance-ids $INSTANCE_ID
+  aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+  IP=`aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text`
+  echo $IP
+  REMOTE="ubuntu@$IP"
+  # Make sure it is initialized
+  scp -o StrictHostKeyChecking=accept-new -i $PEMPATH github_key $REMOTE:.ssh/id_rsa
+  scp -i $PEMPATH known_hosts $REMOTE:.ssh/known_hosts
+  scp -i $PEMPATH -r $HOME/Work/fuzz_chocopy/src/main/java/chocopy/reference/ $REMOTE:
+  ssh -i $PEMPATH $REMOTE "bash -s" < init.sh
+
+  # Run fuzzing sessions (with repetitions) using multiple screens
+  ssh -i $PEMPATH $REMOTE FUZZ_CONFIG=$CONFIG FUZZ_PROCS=\"$PROCS\" FUZZ_ARGS=\"$ARGS\" FUZZ_REP=\"$EXPERIMENT\" "bash -s" < fuzz_run.sh
+  scp -i $PEMPATH -r $REMOTE:$REMOTE_RESULTS_DIR/* $HOME/mu2_summer_remote_results/$CONFIG/
+	aws ec2 stop-instances --instance-ids $INSTANCE_ID
+
+}
+
+i=1
+while read p; do
+  runFuzzing $p $i &
+	i=$(($i+$PROCS))
+done < $INSTANCES_FILE
+wait
+
