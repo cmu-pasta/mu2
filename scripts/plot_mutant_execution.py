3ac926a3228f6522ceb016ad741a0e5f126374d4
Vasu Vikram <vvikram@andrew.cmu.edu>
2022-12-14 14:32:31 -0500

    update requirements

--- scripts/plot_mutant_execution.py
+++ scripts/plot_mutant_execution.py
@@ -2,6 +2,7 @@ from mplcursors import cursor
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import argparse
 
 if __name__ == "__main__":
@@ -11,8 +12,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     df = pd.read_csv(args.mutant_execution_file)
+    df['execution_count_log'] = np.log10(df['execution_count'])
 
-    ax = sns.relplot(data=df, x='execution_count', y='mutant', hue='killed')
+    ax = sns.relplot(data=df, x='execution_count_log', y='mutant', hue='killed')
     plt.title("Mutant Execution Counts")
     plt.xlabel("Execution Count")
     plt.ylabel("Mutants")
